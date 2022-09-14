cb37db22fe86e8d68209baf390e31ede7fe451c4
Rafael <94846981+Faelsil@users.noreply.github.com>
2022-09-13 21:34:15 -0300

    Update ex8.py

--- Exercicios_Primeira Semana/ex8.py
+++ Exercicios_Primeira Semana/ex8.py
@@ -1,3 +1,12 @@
+''''
+O custo ao consumidor de um carro novo é a soma do custo de fábrica com a percentagem do
+distribuidor e dos impostos (aplicados ao custo de fábrica) Supondo que a percentagem do
+distribuidor seja de 28 e os impostos de 45 faça um programa que leia o custo de fábrica de
+um carro e escreva o custo ao consumidor
+
+''''
+
+
 carro = float(input('Digite o Valor do carro: '))
 
 imposto = carro * 28 /100
