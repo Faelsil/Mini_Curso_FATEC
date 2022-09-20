e8af4c8c5e4e9cc47e16a167f611617b48487981
Rafael <94846981+Faelsil@users.noreply.github.com>
2022-09-19 21:39:24 -0300

    Add files via upload

---
+++ Exercicios_Segunda Semana/002.py
@@ -0,0 +1,13 @@
+imposto = float(input('Imposto'))
+
+if imposto < 10.:
+    print('baixo')
+
+elif imposto >= 10 and imposto <= 27.:
+    print('medio')
+
+elif imposto > 27. and imposto < 100:
+    print('alto')
+
+else:
+    print('Imposto invalido')
